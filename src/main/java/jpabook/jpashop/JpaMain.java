8a176752e7f09e13869fc27f13d4b334a9ebc98b
김용환 <ghks4421@gmail.com>
2022-08-22 19:51:24 +0900

    first commit JPA 설계 study

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // MEMBER저장
+        try{
+
+
+            Order order = em.find(Order.class,1L);
+            Long memberId = order.getMemberId();
+
+
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+
+    }
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column( name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    private int oderPrice;
+    private int count;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public void setOderPrice(int oderPrice) {
+        this.oderPrice = oderPrice;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public int getOderPrice() {
+        return oderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
